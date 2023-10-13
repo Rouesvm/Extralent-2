c93ce2e9e5f14750cdf3847bdb644fa85cd64a0a
Drivlo <Rouesvmnowfree@gmail.com>
2023-10-13 22:30:56 +0800

    code

--- src/main/java/com/extralent/common/recipe/recipeTypes/FusingRecipes.java
+++ src/main/java/com/extralent/common/recipe/recipeTypes/FusingRecipes.java
@@ -9,11 +9,7 @@ import net.minecraft.item.ItemStack;
 public class FusingRecipes {
 
     public static void registerRecipes() {
-        RecipeAPI recipe = new RecipeAPI(
-                new ItemStack(ModItems.rydrixIngot),
-                new ItemStack(Items.REDSTONE)
-                , new ItemStack(ModItems.lydrix)
-        );
+        RecipeAPI recipe = new RecipeAPI(new ItemStack(ModItems.rydrixIngot), new ItemStack(Items.REDSTONE), new ItemStack(ModItems.lydrix));
         RecipeHandler.addRecipe(recipe);
     }
 }
