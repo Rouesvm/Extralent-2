b6cca3b40b5a70306d5c65557c11a2d27a4a7ce0
Drivlo <rouesvmnowfree@gmail.com>
2023-11-04 20:12:14 +0800

    Added a new recipe.

--- src/main/java/com/extralent/common/recipe/recipeTypes/FusingRecipes.java
+++ src/main/java/com/extralent/common/recipe/recipeTypes/FusingRecipes.java
@@ -1,12 +1,16 @@
 package com.extralent.common.recipe.recipeTypes;
 
+import com.extralent.common.block.ModBlocks;
 import com.extralent.common.item.ModItems;
 import com.extralent.common.recipe.RecipeHandler;
 import com.google.common.collect.Lists;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 
 public class FusingRecipes {
     public static void registerFusingRecipes() {
         RecipeHandler.addRecipe(Lists.newArrayList(ModItems.rydrixIngot, Items.REDSTONE), ModItems.lydrix);
+        RecipeHandler.addRecipe(Lists.newArrayList(ModBlocks.blockLydrix.getItemBlock(), Item.getItemFromBlock(Blocks.REDSTONE_BLOCK)), ModBlocks.machineCasing.getItemBlock());
     }
 }
